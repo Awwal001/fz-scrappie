01cb84cc7ccf40df6acc6b7ad6fe6dc95a7fd0e5
Awwal Aderibigbe <mac@macs-MacBook-Pro.local>
2022-10-09 10:20:42 +0100

    commiting

---
+++ scraper/urls.py
@@ -0,0 +1,11 @@
+# from django.urls import path
+
+# from . import views
+
+# app_name='scraper'
+
+# urlpatterns = [
+#     path('', views.home, name='index'),
+#     path('generate/', views.generate_download_link, name='generate'),
+# ]
+
